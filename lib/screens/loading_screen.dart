cefff45d612fe484b16b58b4c5f0b8f863af3ff0
Vinicius Vicenzo <vvprockl@outlook.com>
2022-10-26 21:13:50 -0300

    Utilizando o dado de localização para obter a temperatura

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tempo_template/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,17 +11,40 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  Future<void> getLocation() async {
+    Location location = new Location();
+    location.getCurrentLocation();
+  }
+
+  void getData() async {
+    var url = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) { // se a requisição foi feita com sucesso
+      var data = response.body;
+      var jsonData = jsonDecode(data);
+
+      var cityName = jsonData['name'];
+      var temperature = jsonData['main']['temp'];
+      var weatherCondition = jsonData['weather'][0]['id'];
+      print('cidade: $cityName, temperatura: $temperature, condição: $weatherCondition');
+
+    } else {
+      print(response.statusCode);  // senão, imprima o código de erro
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // obtém a localização atual
-          },
-          child: const Text('Obter Localização'),
-        ),
-      ),
     );
   }
 }
